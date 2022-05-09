ec5c797ce2d56d4d6eab225a98cd38bb33cad814
sach-007 <sachinbachhav600@gmail.com>
2022-05-09 22:46:38 +0530

    ODPMS_My

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+            <div class="hk-footer-wrap container">
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+<p>Developed By<a href="" class="text-dark">Sachin & Shubham</a> ©2022</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
